3809246e63087ef247207d252719199d5e536958
Hin <nujaijnah@live.com>
2019-03-11 09:25:38 +0800

    'delete_log'

--- plane.game/canvas/canvas.js
+++ plane.game/canvas/canvas.js
@@ -9,7 +9,6 @@ let drawImage = function(object) {
     if ('image' in object) {
         context.drawImage(object.image, object.x, object.y)
     } else if ('text' in object) {
-        log(object)
         context.fillStyle = "white"
         context.font = "20px serif"
         context.fillText(object.text, object.x, object.y);
